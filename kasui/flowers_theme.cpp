faaa87e2b0a5150644a347d121a02ab27fcf3e36
Mauro Persano <mpr@fzort.org>
2018-04-26 18:44:26 -0300

    clean up flowers theme

--- kasui/flowers_theme.cpp
+++ kasui/flowers_theme.cpp
@@ -1,41 +1,16 @@
-#include <stdlib.h>
+#include <algorithm>
 
 #include "guava2d/g2dgl.h"
 #include "guava2d/texture_manager.h"
 #include "guava2d/vec2.h"
-#include "guava2d/vertex_array.h"
 
-#include "gl_check.h"
 #include "common.h"
 #include "flowers_theme.h"
-#include "program_registry.h"
 #include "render.h"
 
-enum
-{
-    NUM_FLOWERS = 16,
-    FADE_TTL = 30 * MS_PER_TIC,
-};
+constexpr auto FADE_TTL = 30 * MS_PER_TIC;
 
-struct flower
-{
-    using vertex_array_type = g2d::indexed_vertex_array<GLubyte, g2d::vertex::attrib<GLfloat, 2>, g2d::vertex::attrib<GLshort, 2>,
-                                      g2d::vertex::attrib<GLfloat, 1>>;
-
-    float size;
-    g2d::vec2 pos, speed;
-    float angle, delta_angle;
-    int tics, ttl;
-
-    void reset();
-    void draw(vertex_array_type &gv) const;
-    void update(uint32_t dt);
-};
-
-static flower flowers[NUM_FLOWERS];
-static const g2d::texture *ume_texture;
-
-void flower::reset()
+void flowers_theme::flower::reset()
 {
     const float f = 1.f / MS_PER_TIC;
 
@@ -54,43 +29,7 @@ void flower::reset()
     ttl = frand(2 * FADE_TTL, 150 * MS_PER_TIC);
 }
 
-void flower::draw(vertex_array_type &gv) const
-{
-    float alpha_scale;
-
-    if (tics < FADE_TTL)
-        alpha_scale = static_cast<float>(tics) / FADE_TTL;
-    else if (tics > ttl - FADE_TTL)
-        alpha_scale = 1. - static_cast<float>(tics - (ttl - FADE_TTL)) / FADE_TTL;
-    else
-        alpha_scale = 1;
-
-    const float a = alpha_scale * .6;
-
-    const float cur_size = size * powf(1.005, static_cast<float>(tics) / MS_PER_TIC);
-    const float c = cosf(angle) * cur_size;
-    const float s = sinf(angle) * cur_size;
-
-    const g2d::vec2 up = g2d::vec2(c, s);
-    const g2d::vec2 side = g2d::vec2(-s, c);
-
-    const g2d::vec2 p0 = pos + up + side;
-    const g2d::vec2 p1 = pos + up - side;
-    const g2d::vec2 p2 = pos - up - side;
-    const g2d::vec2 p3 = pos - up + side;
-
-    const int vert_index = gv.get_num_verts();
-
-    gv << p0.x, p0.y, 0, 0, a;
-    gv << p1.x, p1.y, 1, 0, a;
-    gv << p2.x, p2.y, 1, 1, a;
-    gv << p3.x, p3.y, 0, 1, a;
-
-    gv < vert_index + 0, vert_index + 1, vert_index + 2;
-    gv < vert_index + 2, vert_index + 3, vert_index + 0;
-}
-
-void flower::update(uint32_t dt)
+void flowers_theme::flower::update(uint32_t dt)
 {
     const g2d::vec2 cur_speed = speed * powf(1.005, static_cast<float>(tics) / MS_PER_TIC);
 
@@ -109,66 +48,58 @@ void flower::update(uint32_t dt)
 }
 
 flowers_theme::flowers_theme()
+    : texture_{g2d::texture_manager::get_instance().load("images/ume.png")}
 {
-    ume_texture = g2d::texture_manager::get_instance().load("images/ume.png");
+    reset();
 }
 
 void flowers_theme::reset()
 {
-    for (auto& flower : flowers)
+    for (auto& flower : flowers_)
         flower.reset();
 }
 
 void flowers_theme::update(uint32_t dt)
 {
-    for (auto& flower : flowers)
+    for (auto& flower : flowers_)
         flower.update(dt);
-}
-
-static int flower_z_compare(const void *foo, const void *bar)
-{
-    const float s0 = (*(flower **)foo)->size;
-    const float s1 = (*(flower **)bar)->size;
-    return s0 < s1 ? -1 : 1;
 }
 
 void flowers_theme::draw() const
 {
-    render::end_batch();
+    render::set_blend_mode(blend_mode::ALPHA_BLEND);
 
-    // HACK
-    GL_CHECK(glBindBuffer(GL_ARRAY_BUFFER, 0));
-    GL_CHECK(glBindVertexArray(0));
+    for (const auto& p : flowers_) {
+        const auto tics = p.tics;
+        const auto ttl = p.ttl;
 
-    static flower *sorted_flowers[NUM_FLOWERS];
+        float alpha_scale;
+        if (tics < FADE_TTL)
+            alpha_scale = static_cast<float>(tics) / FADE_TTL;
+        else if (tics > ttl - FADE_TTL)
+            alpha_scale = 1. - static_cast<float>(tics - (ttl - FADE_TTL)) / FADE_TTL;
+        else
+            alpha_scale = 1;
 
-    for (int i = 0; i < NUM_FLOWERS; i++)
-        sorted_flowers[i] = &flowers[i];
+        const float a = alpha_scale * .6;
 
-    qsort(sorted_flowers, NUM_FLOWERS, sizeof *sorted_flowers, flower_z_compare);
+        const float cur_size = p.size * powf(1.005, static_cast<float>(tics) / MS_PER_TIC);
+        const float c = cosf(p.angle) * cur_size;
+        const float s = sinf(p.angle) * cur_size;
 
-    static flower::vertex_array_type gv(NUM_FLOWERS * 4, NUM_FLOWERS * 6);
+        const g2d::vec2 up = g2d::vec2(c, s);
+        const g2d::vec2 side = g2d::vec2(-s, c);
 
-    gv.reset();
+        const auto& pos = p.pos + .5 * g2d::vec2{window_width, window_height};
+        const g2d::vec2 p0 = pos + up + side;
+        const g2d::vec2 p1 = pos + up - side;
+        const g2d::vec2 p2 = pos - up - side;
+        const g2d::vec2 p3 = pos - up + side;
 
-    for (auto &sorted_flower : sorted_flowers)
-        sorted_flower->draw(gv);
-
-    const g2d::mat4 proj_modelview =
-        get_ortho_projection() * g2d::mat4::translation(.5 * window_width, .5 * window_height, 0);
-
-    glEnable(GL_BLEND);
-    glBlendFunc(GL_SRC_ALPHA, GL_ONE_MINUS_SRC_ALPHA);
-
-    ume_texture->bind();
-
-    program_texture_alpha &prog = get_program_instance<program_texture_alpha>();
-    prog.use();
-    prog.set_proj_modelview_matrix(proj_modelview);
-    prog.set_texture(0);
-
-    gv.draw(GL_TRIANGLES);
-
-    render::begin_batch();
-    render::set_viewport(0, window_width, 0, window_height);
+        render::set_color({1, 1, 1, a});
+        render::draw_quad(texture_,
+                {p0, p1, p2, p3},
+                {{0, 0}, {1, 0}, {1, 1}, {0, 1}},
+                -99);
+    }
 }
--- kasui/flowers_theme.h
+++ kasui/flowers_theme.h
@@ -2,6 +2,8 @@
 
 #include "theme.h"
 
+#include <array>
+
 class flowers_theme : public theme_animation
 {
 public:
@@ -10,4 +12,19 @@ public:
     void reset() override;
     void draw() const override;
     void update(uint32_t dt) override;
+
+private:
+    struct flower
+    {
+        float size;
+        g2d::vec2 pos, speed;
+        float angle, delta_angle;
+        int tics, ttl;
+
+        void reset();
+        void update(uint32_t dt);
+    };
+
+    std::array<flower, 16> flowers_;
+    const g2d::texture *texture_;
 };
